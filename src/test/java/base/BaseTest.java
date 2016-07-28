a2a47b807836d67629ee683913ce738a84c98434
Yu Yufeng <Yu Yufeng@yyf>
2016-07-28 21:17:09 +0800

    日志记录到文件、数据库中

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,17 +1,17 @@
 package base;
 
-
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * Created with IntelliJ IDEA.
- * Description:Dao 测试基类
- * 配置Spring和Junit整合,junit启动时加载springIOC容器
- * spring-test,junit
+ * Created with IntelliJ IDEA. Description:Dao 测试基类
+ * 配置Spring和Junit整合,junit启动时加载springIOC容器 spring-test,junit
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml"})
+@ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
 public abstract class BaseTest {
+	
 }
--- src/test/java/com/lib/dao/UserInfoDaoTest.java
+++ src/test/java/com/lib/dao/UserInfoDaoTest.java
@@ -4,23 +4,36 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.lib.entity.UserInfo;
+
+import base.BaseTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:spring/spring-dao.xml" })
 public class UserInfoDaoTest {
 	@Resource
 	private UserInfoDao userInfoDao;
+	private final static Logger LOG = LoggerFactory.getLogger(UserInfoDaoTest.class);
 
 	@Test
 	public void testQueryById() throws Exception {
+		LOG.info("logback 成功了");
 		long id = 2016001l;
 		UserInfo user = userInfoDao.queryById(id);
 		System.out.println(user);
 
+	}
+	
+	@Test
+	public void test() throws Exception {
+		LOG.info("logback 成功了");
+		LOG.debug("debug");
+		LOG.error("dsa");
 	}
 
 }
